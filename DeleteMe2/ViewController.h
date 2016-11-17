79e07423160b335ee2fa03fbc559809cc3182f49
Elliott D'alvarez <elliott.dalvarez@btinternet.com>
2016-11-17 14:09:54 +0000

    Initial Commit

---
+++ DeleteMe2/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DeleteMe2
+//
+//  Created by Elliott D'Alvarez on 17/11/2016.
+//  Copyright © 2016 Test. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ DeleteMe2/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  DeleteMe2
+//
+//  Created by Elliott D'Alvarez on 17/11/2016.
+//  Copyright © 2016 Test. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
